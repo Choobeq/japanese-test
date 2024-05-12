65610f9924109299c28df375188b47d779ed249a
Tomasz Wisniewski <tsz.wisniewski@gmail.com>
2024-05-12 16:21:38 +0100

    Update japanese_test.py
    Added check if the file 'statistics.csv' exists.

--- japanese_test.py
+++ japanese_test.py
@@ -1,8 +1,9 @@
-# This is a test to practice Japanese Vocabulary for black belt exam.
+# Test to practice Japanese Vocabulary for Black Belt Exam.
 # qn = question number
 # ua = user answer
 
-import random
+# Importing libraries
+import random, os
 from datetime import date, datetime
 
 # Declaring variables
@@ -22,22 +23,23 @@ with open('questions.csv') as f1:
 with open('answers.csv') as f2:
     answers = f2.read().splitlines()
 
-#for z in range(0,10):
+# Main loop to ask 10 questions
 while counting_steps < 10:
     qn = random.randint(0,95)
-    if questions[qn] in temp_questions:
+    if questions[qn] in temp_questions: # Checking if the question was already in this run.
         continue
     else:
         counting_steps += 1
         temp_questions.append(questions[qn])
         print(f"\nQuestion number {counting_steps}\t: {questions[qn]}")
         ua = input('Your answer here\t: ').upper()
+        
+        # Checking each word separately to identify if words are correct but in different order
         diff_set = set(answers[qn].split()).symmetric_difference(set(ua.split()))
         if ua == answers[qn]:
             print('Correct!')
             counting_correct_answers += 1
         elif len(diff_set) == 0:
-            #print(diff_set)
             print('Correct words but different order!')
             print(f'Order in database\t: {answers[qn]}')
             counting_correct_answers += 1
@@ -48,16 +50,22 @@ while counting_steps < 10:
 # Printing message on the screen with the score.
 print(f'You Have Scored {counting_correct_answers} Points!')
 
-# Checking if the score is single digit and converting to double digit if needed.
+# Checking if the score is single digit and converting it to double digits if needed.
 if len(str(counting_correct_answers)) < 2:
     digits = "0"+str(counting_correct_answers)    
 else:
     digits = counting_correct_answers
 
-current_time = datetime.now().strftime("%H:%M")
+# Checking if the file 'statistics.csv' exists,
+# and creates one if it doesn't. 
+if not os.path.exists('statistics.csv'):
+    with open('statistics.csv', "w", encoding="utf-8") as statistics:
+        statistics.write("┌────────────┬───────┬───────┐\n")
+        statistics.write("│    DATE    │ TIME  │ SCORE │\n")
+        statistics.write("├────────────┼───────┼───────┤\n")
 
 # Saving current score to the file "statistics.csv"
-with open('statistics.csv', 'a') as f3:
+with open('statistics.csv', 'a', encoding="utf-8") as f3:
     current_time = datetime.now().strftime("%H:%M")
-    f3.write(F"| {today} | {current_time} |   {digits}  |\n")
-    f3.write(F"|------------|-------|-------|\n")
+    f3.write(f"│ {today} │ {current_time} │   {digits}  │\n")
+    f3.write(f"├────────────┼───────┼───────┤\n")
